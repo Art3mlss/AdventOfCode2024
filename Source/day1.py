09efc852a0727a61f6bbdc4abd62c5236cf3e199
Tom Ricalens <ricatom2003@gmail.com>
2024-12-07 14:03:41 +0100

    day1 complete and commented

--- Source/day1.py
+++ Source/day1.py
@@ -1,11 +1,9 @@
-## ----- Accessing Input
-from itertools import count
+## ----- Accessing Input File
 
 path = "../Resources/"
 day = "1"
 with open(path+"day"+day+".txt", 'r') as file:
     content = file.read()
-    print(content)
 
 ## ----- Part #1
 # Find the total sum of the distances between each item of the two lists sorted
@@ -18,6 +16,7 @@ list1.sort()
 list2.sort()
 
 def findDistance(l1, l2):
+    # Calculate the absolute distance between corresponding elements of two lists
     distances = []
     for i in range(len(list1)):
         distances.append(abs(l1[i] - l2[i]))
@@ -27,17 +26,20 @@ print(sum(findDistance(list1, list2)))
 # Solution for my input : 1590491
 
 ## ----- Part #2
-# Write a function that counts how many times each value of the first list appears in the second list
+# Find how often each item of the first list appears in the second list, then compute the similarity score
 
 def countOccurrences(list1, list2):
+    # Create a dictionary to count occurrences of each item in list1 within list2
     count_dict = {item: list2.count(item) for item in list1}
     return count_dict
 
-def computeScore(list1, list2):
+def computeSimilarityScore(list1, list2):
+    # Compute
     score = 0
     count_dict = countOccurrences(list1, list2)
     for key, value in count_dict.items():
         score += key*value*list1.count(key)
     return score
 
-print(computeScore(list1, list2))
+print(computeSimilarityScore(list1, list2))
+# Solution for my input : 22588371
