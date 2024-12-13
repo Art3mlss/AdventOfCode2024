fdb4e8dcfec6c27ace4d29c9d20344b746dbf7f8
Tom Ricalens <ricatom2003@gmail.com>
2024-12-13 01:03:52 +0100

    day 9 completed and commented
    part 1 run in 0.027s
    part 2 run in 0.97s
    on my laptop

--- Source/day9.py
+++ Source/day9.py
@@ -62,14 +62,91 @@ def computeSum(line):
     res = 0
     for i, elem in enumerate(line):
         if elem == '.':
-            break
+            continue
         res += elem*i
     return res
 
 start = time.time()
 
 decodedLine, freeIndexes = decode(list(map(int, list(content))))
-print(computeSum(moveLeft(decodedLine, freeIndexes)))
+print(computeSum(moveLeft(decodedLine.copy(), freeIndexes)))
 print(f"Execution time : {time.time()-start}")
 # Solution for my input : 6463499258318
 # Ran in 0.027s on my laptop
+
+## ----- Part #2
+# Same as part 1 but move a whole block of numbers to the left instead of one by one
+
+def findFirstBlock(freeIndexes, length):
+    # Find the first block of free consecutive indexes of a given length
+
+    blockLength = 0
+    firstIndex = 0
+    for i, elem in enumerate(freeIndexes):
+        if i==0:
+            blockLength = 1
+            firstIndex = elem
+        else:
+            if elem == freeIndexes[i-1]+1:
+                blockLength += 1
+            else:
+                blockLength = 1
+                firstIndex = elem
+
+        if blockLength == length:
+            return firstIndex
+
+    return -1
+
+def findIdInfos(decodedLine):
+    # Find for each id in the decoded line, its first index and its length
+    # Index of resulting list is the id, value is a list of [firstIndex, length]
+    # Ex : [0, 1, 1, 2, 2, 2] -> [[0, 1], [1, 2], [3, 3]]
+
+    # It could have been a bit more efficient to do this in the decode function
+
+    currentValue = decodedLine[0]
+    currentLength = 1
+    currentFirstIndex = 0
+    res = []
+
+    i = 1
+    while i < len(decodedLine):
+        if decodedLine[i] == currentValue:
+            currentLength += 1
+        else:
+            if currentValue != '.':
+                res += [[currentFirstIndex, currentLength]]
+            currentValue = decodedLine[i]
+            currentLength = 1
+            currentFirstIndex = i
+
+        i += 1
+
+    if currentValue != '.':
+        res += [[currentFirstIndex, currentLength]]
+
+    return res
+
+def moveLeftBlock(decodedLine, freeIndexes):
+    # Move each block of numbers on the right (starting from the right) of decodedLine
+    # to the first free block large enough on the left
+
+    idLocations = findIdInfos(decodedLine)
+    n = len(idLocations)
+    for i, (firstIndex, length) in enumerate(reversed(idLocations)):
+
+        firstPlace = findFirstBlock(freeIndexes, length)
+        if firstPlace != -1 and firstPlace < firstIndex:
+            for i in range(length):
+                decodedLine[firstPlace+i] = decodedLine[firstIndex+i]
+                decodedLine[firstIndex+i] = '.'
+
+                freeIndexes.remove(firstPlace+i)
+    return decodedLine
+
+start2 = time.time()
+print(computeSum(moveLeftBlock(decodedLine.copy(), freeIndexes)))
+print(f"Execution time : {time.time()-start2}")
+# Solution for my input : 6493634986625 .
+# Ran in 2.19s on my laptop
